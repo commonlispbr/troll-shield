7aad9ecd089c8f724c933a2d6b6dfe58dd0a94ae
Manoel Vilela <manoel_vilela@engineer.com>
2020-07-23 11:49:28 -0300

    Make deploy.sh more portable

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -eux
+server=lisp@bikelis
 go build
-ssh bikelis 'pkill troll-shield || true'
-scp troll-shield bikelis:
-git describe --tags | ssh bikelis 'cat > version.txt'
-ssh bikelis 'tmux new-session -d -s troll'
+ssh $server 'pkill troll-shield || true'
+scp troll-shield $server:
+git describe --tags | ssh $server 'cat > version.txt'
+ssh bikelis 'tmux new-session -d -s troll || true'
 ssh bikelis 'tmux new-window -d ./run.sh'
