42a883ca8bc2eb12924a65ac1a1ba5083c13e1aa
Manoel Vilela <manoel_vilela@engineer.com>
2020-08-13 17:01:09 -0300

    Add scheme to set bot commands via API

--- debug/debug.py
+++ debug/debug.py
@@ -1,18 +1,42 @@
+# coding: utf-8
+
 from telepot import Bot
 from pprint import pprint
 
 bot = Bot(open("token.txt").read().strip())
-users = {
-    "lerax": 336558555,
-    "tretanews": 430571154,
-}
 
-chats = {
-    "commonlispbr": -1001280636766,
-    "commonlisphq": -1001493125566,
-}
+def get_chat_debug():
+    users = {
+        "lerax": 336558555,
+        "tretanews": 430571154,
+    }
 
-chat_member = bot.getChatMember(chats["commonlisphq"],
-                                users["tretanews"])
-print()
-pprint(chat_member)
+    chats = {
+        "commonlispbr": -1001280636766,
+        "commonlisphq": -1001493125566,
+    }
+
+    chat_member = bot.getChatMember(chats["commonlisphq"],
+                                    users["tretanews"])
+    print()
+    pprint(chat_member)
+
+
+def set_bot_commands():
+    commands = [
+        {
+            "command": "kills",
+            "description": "Retorna a quantidade de trolls decapitados."
+        },
+        {
+            "command": "lelerax",
+            "description": "Ping. Verifica se estou vivo."
+        },
+        {
+            "command": "pass",
+            "description": "Commando de passe /pass <@username>. Só funciona com os admins do @commonlispbr."
+        }
+    ]
+
+    bot.setMyCommands(commands)
+    pprint(bot.getMyCommands())
